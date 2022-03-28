50c463b9071e3a7decfe17a0246f4849fafb691e
Sonia Ndonga <soniandonga@yahoo.com>
2022-03-28 22:50:48 +0100

    Coded web design fow web design

--- NFT-Marketplace/resources/views/layouts/app.blade.php
+++ NFT-Marketplace/resources/views/layouts/app.blade.php
@@ -7,24 +7,28 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Trendz') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar-trendz navbar navbar-expand-md navbar-light shadow-sm ">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{-- {{ config('app.name', 'Trendz') }} --}}
+
+                    <!--Image Logo Section for the Homepage-->
+                    <img src="{{ asset('img/TRENDZ.jpg') }}" alt="">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -32,23 +36,40 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                         <li class="nav-item">
+          <a class="nav-link nav-trendz" aria-current="page" href="#">Explore</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link nav-trendz" href="#">Stats</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link nav-trendz" href="#">Resources</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link nav-trendz" href="#">Create</a>
+        </li>
+ </ul>
 
-                    </ul>
+        <!--Search button and input on navbar-->
+                    <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-search" type="submit">Search</button>
+      </form>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <li class="nav-item me-2">
+                                    <a class="nav-link btn btn-trendz-login nav-trendz-login" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                <li class="nav-item me-2">
+                                    <a class="nav-link btn btn-trendz-register nav-trendz-register" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
@@ -74,7 +95,6 @@
                 </div>
             </div>
         </nav>
-
         <main class="py-4">
             @yield('content')
         </main>
--- NFT-Marketplace/resources/views/welcome.blade.php
+++ NFT-Marketplace/resources/views/welcome.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
+<body class="trendz-homepage">
+    <section>
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row justify-content-center gx-0">
+        {{-- <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Welcome!</div>
 
@@ -23,7 +25,45 @@
                     Read more <a href="{{route('about')}}">About Us</a>
                 </div>
             </div>
+        </div> --}}
+
+        <div class="col-sm order-sm-first">
+            <div class="col-12 col-sm-12 offset-1 offset-sm-4 h-75 py-5 text-right d-flex flex-column justify-content-center align-items-center">
+            <h1 class="trendz-title">Explore, Buy, Collect and Sell Unqiue NFT's</h1>
+            <h3 class="trendz-sub-title">Trendz is a website where who can find any possible NFT's in the marketplace </h3>
+
+            <a class="btn explore-button px-3 m-4 d-flex flex-column me-md-3" href="#">Explore</a>
+        </div>
+         </div>
+
+        <div class="col-sm">
+         <div class="col-md-6 order-md-6">
+            <!--Image Section for the Homepage-->
+                    <img class="img-fluid trendz-home-img" src="{{ asset('img/astronaut.png') }}" alt="">
+        </div>
+        </div>
+
+
+</div>
+</div>
+    </section> <!--End of Homepage Image and Text Section-->
+    <div class="container">
+        <div class="row justify-content-center gx-0">
+            <div class="col-sm order-sm-first">
+                <div class="col-8 col-sm-8 offset-1 offset-sm-4 h-75 py-5 text-right d-flex flex-column justify-content-start align-items-right">
+                    <h3>To find more about NFT's and how it works</h3>
+                    <p>Click the button to explore more</p>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="col-md-6 order-md-6">
+                    <a class="btn NFT-button px-3 m-4 d-flex flex-column me-md-3" href="#">NFT's</a>
+                </div>
+            </div>
         </div>
     </div>
-</div>
+    <section>
+
+    </section>
+</body>
 @endsection
