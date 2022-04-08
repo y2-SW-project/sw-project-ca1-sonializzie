2a949ae3c171ddd0849e9afa92a525e96f16cc0a
Sonia Ndonga <soniandonga@yahoo.com>
2022-04-08 20:25:33 +0100

    Styled using css and blade.php for login and register

--- NFT-Marketplace/resources/views/user/home.blade.php
+++ NFT-Marketplace/resources/views/user/home.blade.php
@@ -5,16 +5,16 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header user-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
-                    You are logged in as an Ordinary User!
+                    <p class="user-text">You are logged in as an Ordinary User!</p>
                 </div>
             </div>
         </div>
